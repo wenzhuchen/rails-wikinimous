dde34dc2db2ef0ff4bb8dd73724889538eda3453
wenzhu chen <wenzhu.chen1@gmail.com>
2017-09-29 15:22:02 +0200

    completed the crud

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+10.times do |x|
+  Article.create(
+    title: Faker::Cat.breed,
+    content: Faker::Lorem.paragraph(3, true)
+    )
+end
+
